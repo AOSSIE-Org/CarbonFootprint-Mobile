5932da0b750429077c029bf44e390958adbf9fb5
Amit Kanwar <kanwaramit92@gmail.com>
2017-06-19 14:47:19 +0530

    Splash Screen For iOS

--- app/index.js
+++ app/index.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
+
 import store from './config/store';
 import Router from './config/routes';
 
+
 class CarbonFootprint extends Component {
+    componentDidMount() {
+        console.log("Starting Splash Screen");
+        SplashScreen.hide();
+        console.log("Ending Splash Screen");
+    }
+
     render() {
         return(
             <Provider store={store}>
@@ -12,13 +21,5 @@ class CarbonFootprint extends Component {
         )
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-});
 
 export default CarbonFootprint;
