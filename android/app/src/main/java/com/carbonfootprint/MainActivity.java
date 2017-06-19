c0b83b66cde20c88de904df07a9e5fb8cfb93d21
Amit Kanwar <kanwaramit92@gmail.com>
2017-06-19 14:57:15 +0530

    Splash Screen for Android

--- android/app/src/main/java/com/carbonfootprint/MainActivity.java
+++ android/app/src/main/java/com/carbonfootprint/MainActivity.java
@@ -1,6 +1,7 @@
 package com.carbonfootprint;
 
 import com.facebook.react.ReactActivity;
+import com.cboy.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -8,6 +9,11 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
     @Override
     protected String getMainComponentName() {
         return "CarbonFootprint";
