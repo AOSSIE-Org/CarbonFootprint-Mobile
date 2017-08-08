0545f1d19898ea2bebc8e9f68caed834a90cfd09
Amit Kanwar <kanwaramit92@gmail.com>
2017-08-09 00:03:01 +0530

    Fixing FB login for Facebook

--- app/actions/FbLoginAction.js
+++ app/actions/FbLoginAction.js
@@ -3,6 +3,8 @@
 */
 import FBSDK, { LoginManager, LoginButton, AccessToken } from 'react-native-fbsdk';
 import * as firebase from 'firebase';
+import { Actions, ActionConst } from 'react-native-router-flux';
+
 import { receiveAuth } from './AuthAction';
 
 export function fbLogin() {
@@ -22,18 +24,6 @@ export function fbLogin() {
 			              Actions.calculate();
 					  })
 				  }
-		          /*
-				  (data) => {
-		          	fetch('https://graph.facebook.com/v2.5/me?fields=email,name&access_token=' + data.accessToken)
-					  .then((response) => response.json())
-					  .then((json) => {
-
-					  })
-					  .catch((error) => {
-					    alert('FB ERROR GETTING DATA FROM FACEBOOK : ' + error);
-					  })
-		          }
-				  */
 		       )
 		    }
 		  },
