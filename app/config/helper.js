8efd3855ca39b76264675e81f3bca1eaca560473
Anuj Gupta <anujgupta2004@gmail.com>
2017-08-13 11:20:01 +0530

    Adding link to Google Activity detection API

--- app/config/helper.js
+++ app/config/helper.js
@@ -9,6 +9,8 @@ export function getIcon(name) {
 
 export function getIconName(activity) {
 	// Selecting activity icon based on detected activity
+  // For more information about detected activity, check below link -
+  // https://developers.google.com/android/reference/com/google/android/gms/location/DetectedActivity
     var icon;
     switch(activity) {
       case 'IN_VEHICLE': {
