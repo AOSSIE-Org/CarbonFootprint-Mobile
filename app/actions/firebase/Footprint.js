34405b62f399c3ce1d1e2fe16a5fb6e79984831a
Amit Kanwar <kanwaramit92@gmail.com>
2017-08-20 16:39:45 -0400

    Adding Footprint Firebase API

---
+++ app/actions/firebase/Footprint.js
@@ -0,0 +1,51 @@
+import * as firebase from 'firebase';
+import {
+    getUser
+} from './User';
+
+/*
+    Do all the calculations locally. Query Firebase only to set (not to update).
+    Example Data Object to set.
+
+    data = {
+        total: {
+            time:
+            footprint:
+            distance:
+        },
+        car: {
+            time:
+            footprint:
+            distance:
+        },
+        transit: {
+            time:
+            footprint:
+            distance:
+        },
+        cycling: {
+            time:
+            footprint:
+            distance:
+        },
+        walking: {
+            time:
+            footprint:
+            distance:
+        },
+    }
+
+    currentUid: UID of the logged in user to set.
+*/
+export function setFootprint(data, currentUid) {
+    return new Promise(function(resolve, reject) {
+        firebase.database().ref('users/' + currentUid)
+        .child("data").setValue(...data)
+        .then(() => {
+            getUser(currentUid)
+            .then((user) => resolve(user))
+            .catch((error) => reject(error))
+        })
+        .catch((error) => reject(error))
+    });
+}
