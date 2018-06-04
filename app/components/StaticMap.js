6eb48e6a7eb6efa177fb1711a6ac2e31652a08dd
Madhav <madhavgaba2@gmail.com>
2018-06-04 11:38:02 +0530

    Added location button in calculate screen

--- app/components/StaticMap.js
+++ app/components/StaticMap.js
@@ -1,55 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Button
+} from 'react-native';
 
-/**
- *  Map to show Users
- * @param  props properties from parent class
- */
-const StaticMap = props => {
-    const markers = (location, id) => {
-        let color = !id ? 'red' : 'green';
+class StaticMap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { statusBarHeight: 60 };
+    }
+
+    markers(location, id) {
+        let color = !id ?
+            "red": "green";
         return (
-            <MapView.Marker
-                coordinate={{
-                    latitude: location.latitude,
-                    longitude: location.longitude
-                }}
-                pinColor={color}
-            />
-        );
-    };
+            <MapView.Marker coordinate={{
+                latitude: location.latitude,
+                longitude: location.longitude
+            }} pinColor={color}/>
+        )
+    }
+    componentWillMount() {
+     //Hack to ensure the showsMyLocationButton is shown initially. Idea is to force a repaint
+        setTimeout( () => {
+            this.setState({ statusBarHeight: 110 })
+        });
+    }
 
-    return (
-        <MapView
-            provider={PROVIDER_GOOGLE}
-            showsUserLocation={true}
-            showsMyLocationButton={true}
-            region={props.region}
-            style={styles.map}
-        >
-            {props.source ? markers(props.source, 0) : null}
-            {props.destination ? markers(props.destination, 1) : null}
-            {props.coords ? (
-                <MapView.Polyline
-                    coordinates={props.coords}
-                    strokeWidth={4}
-                    strokeColor="#666"
-                />
-            ) : null}
-        </MapView>
-    );
-};
+    render() {
+        let props = this.props;
+        return(
+            <View style={[styles.mapContainer, {marginTop: this.state.statusBarHeight}]}>
+                <MapView provider={PROVIDER_GOOGLE}
+                    showsUserLocation={true}
+                    showsMyLocationButton={true}
+                    showsCompass={true}
+                    region={props.region} style={styles.map}>
+                    {
+                        props.source ?
+                        this.markers(props.source, 0): null
+                    }
+                    {
+                        props.destination ?
+                        this.markers(props.destination, 1): null
+                    }
+                    {
+                        props.coords ?
+                        <MapView.Polyline
+                            coordinates={props.coords}
+                            strokeWidth={4}
+                            strokeColor="#666" />
+                        : null
+                    }
+                </MapView>
+            </View>
+        )
+    }
+}
 
-/*StyleSheet*/
 const styles = StyleSheet.create({
+    mapContainer: {
+        position: 'absolute',
+        top: 0,
+        right: 0, 
+        left: 0, 
+        bottom: 0,
+        height: 430
+    },
     map: {
         top: 0,
         bottom: 0,
         left: 0,
         right: 0,
-        position: 'absolute'
-    }
-});
+        position: 'absolute',
+    },
+})
+
 
 export default StaticMap;
