c82beb19ede408f315d83057fae1aa596d9b6fcb
Saad Pasta <saadpasta70@gmail.com>
2018-11-12 16:26:37 +0000

    Upload New File

---
+++ app/components/OfflineNotice.js
@@ -0,0 +1,59 @@
+import React, { PureComponent } from 'react';
+import { View, Text, NetInfo, Dimensions, StyleSheet } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+function MiniOfflineSign() {
+  return (
+    <View style={styles.offlineContainer}>
+      <Text style={styles.offlineText}>No Internet Connection</Text>
+    </View>
+  );
+}
+
+class OfflineNotice extends PureComponent {
+  state = {
+    isConnected: true
+  };
+
+  componentDidMount() {
+    NetInfo.isConnected.addEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  componentWillUnmount() {
+    NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+  }
+
+  handleConnectivityChange = isConnected => {
+    if (isConnected) {
+      this.setState({ isConnected });
+    } else {
+      this.setState({ isConnected });
+    }
+  };
+
+  render() {
+    if (!this.state.isConnected) {
+      return <MiniOfflineSign />;
+    }
+    return null;
+  }
+}
+
+const styles = StyleSheet.create({
+  offlineContainer: {
+    backgroundColor: '#b52424',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width,
+    marginRight:30,
+    marginLeft:-13,
+    position: 'absolute',
+    top: 5
+  },
+  offlineText: { color: '#fff' }
+});
+
+export default OfflineNotice;
