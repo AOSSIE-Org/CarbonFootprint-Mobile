f8a4c7494872f35527fcc92fa7c9dfc4370b13ea
Saad Pasta <saadpasta70@gmail.com>
2018-12-09 03:50:23 +0000

    About Us Page Updated Better UI

--- app/containers/About.js
+++ app/containers/About.js
@@ -1,111 +1,171 @@
 import React from 'react';
 import {
-    View,
-    StyleSheet,
-    Text,
-    Dimensions,
-    Image,
-    StatusBar,
-    TouchableHighlight,
-    Alert,
-    Linking
+	View,
+	StyleSheet,
+	Text,
+	Dimensions,
+	Image,
+	StatusBar,
+	TouchableHighlight,
+	Alert,
+	Linking,
+	ScrollView,
+	TouchableOpacity
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-
+import {
+	aossieTitle,
+	aossieDescription,
+	appDescription
+} from '../config/constants';
 import Header from '../components/Header';
 import images from '../config/images';
-
+const { width, height } = Dimensions.get('window');
 const About = () => {
-    return(
-        <View style={styles.container}>
-            <Header icon={true} iconName="arrow-back" text="About Us" />
-            <View style={styles.main}>
-                <Image source={images.logo} style={styles.image} />
-                <Text style={styles.large}> Carbon Footprint</Text>
-                <View style={styles.text}>
-                    <View style={styles.small}>
-                        <Text>
-                            Carbon Footprint is an open-source project developed by
-                        </Text>
-                        <Text style={styles.blueText}
-                                onPress={() => Linking.openURL('http://aossie.org')}>
-                            AOSSIE
-                        </Text>
-                        <Text>
-                        (the Australian Open Source Software Innovation and Education association). 
-                            This mobile application calculates amount of emitted co2 based on user's activity and traveled distance. 
-                            Method used to calculate co2 emission is given 
-                            </Text>
-                            <TouchableHighlight onPress={this.calcMethod}>
-                                <Text style={styles.blueText}>
-                                here
-                                </Text>    
-                        </TouchableHighlight>
-                        <Text>
-                        . Feedbacks and contributions are welcome in our 
-                        </Text>
-                        <Text style={styles.blueText} 
-                            onPress={() => Linking.openURL('https://gitlab.com/aossie/CarbonFootprint-Mobile')}>
-                            GitLab repository
-                        </Text>
-                        <Text>
-                        . Report bugs and request features in our 
-                        </Text>
-                        <Text style={styles.blueText}
-                                onPress={() => Linking.openURL('https://gitlab.com/aossie/CarbonFootprint-Mobile/issues')}>
-                            issue tracker.
-                        </Text>
-                    </View>
-                </View>
-            </View>
-        </View>
-    )
+	return (
+		<View style={styles.container}>
+			<ScrollView>
+				<Header icon={true} iconName="arrow-back" text="About Us" />
+				<View style={styles.main}>
+					<Image source={images.aossie} style={styles.image} />
+					<Text style={styles.title}>{aossieTitle}</Text>
+					<Text style={styles.description}>{aossieDescription}</Text>
+					<View style={styles.divider} />
+					<Image source={images.logo} style={styles.image} />
+					<Text style={styles.large}>Carbon Footprint</Text>
+					<View style={styles.text}>
+						<View>
+							<Text style={styles.small}>{appDescription}</Text>
+						</View>
+					</View>
+					<View style={styles.divider} />
+					<Text style={styles.title}>
+						Follow us and contribute to our project!
+					</Text>
+					<View style={styles.social}>
+						<TouchableOpacity
+							onPress={() =>
+								Linking.openURL(
+									'https://gitlab.com/aossie/CarbonFootprint-Mobile'
+								)
+							}
+						>
+							<Image
+								style={styles.socialItem}
+								resizeMethod="resize"
+								source={images.gitlab}
+							/>
+						</TouchableOpacity>
+						<TouchableOpacity
+							onPress={() =>
+								Linking.openURL(
+									'https://groups.google.com/forum/#!forum/aossie-gsoc-2017'
+								)
+							}
+						>
+							<Image
+								style={styles.socialItem}
+								resizeMethod="resize"
+								source={images.gsoc}
+							/>
+						</TouchableOpacity>
+						<TouchableOpacity
+							onPress={() => Linking.openURL('https://twitter.com/aossie_org')}
+						>
+							<Image
+								style={styles.socialItem}
+								resizeMethod="resize"
+								source={images.twitter}
+							/>
+						</TouchableOpacity>
+						<TouchableOpacity
+							onPress={() => Linking.openURL('https://aossie.gitlab.io/')}
+						>
+							<Image
+								style={styles.socialItem}
+								resizeMethod="resize"
+								source={images.webiste}
+							/>
+						</TouchableOpacity>
+					</View>
+				</View>
+			</ScrollView>
+		</View>
+	);
 
-    const calcMethod = () => {
-        Alert.alert(
-            'co2 calculation method',
-            'Present formula for calculating co2 emission is: emitted co2 (in kg) = co2 emission rate of fuel (in kg/L) * (traveled distance (in km) / mileage of vehicle (in km/L)). If activity is IN_VEHICLE, co2 calculated by above formula becomes Emitted co2 and Saved co2 is 0. If activity is WALKING, ON_BICYCLE etc., co2 calculated by above formula becomes Saved co2 and Emitted co2 is 0.',
-            [
-                {
-                    text: 'OK'
-                }
-            ],
-            { cancelable: true }
-        );  
-    }
-}
+	const calcMethod = () => {
+		Alert.alert(
+			'co2 calculation method',
+			'Present formula for calculating co2 emission is: emitted co2 (in kg) = co2 emission rate of fuel (in kg/L) * (traveled distance (in km) / mileage of vehicle (in km/L)). If activity is IN_VEHICLE, co2 calculated by above formula becomes Emitted co2 and Saved co2 is 0. If activity is WALKING, ON_BICYCLE etc., co2 calculated by above formula becomes Saved co2 and Emitted co2 is 0.',
+			[
+				{
+					text: 'OK'
+				}
+			],
+			{ cancelable: true }
+		);
+	};
+};
 
 //StyleSheet
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginTop: StatusBar.currentHeight
-    },
-    main: {
-        alignItems: 'center',
-        marginTop: 65, // (45 + 20)
-        padding: 13
-    },
-    image: {
-        width: 80,
-        height: 80
-    },
-    large: {
-        letterSpacing: 2,
-        fontSize: 14,
-        marginTop: 10
-    },
-    small: {
-        fontSize: 13,
-        color: '#555',
-        padding: 13
-    },
-    text: {
-        marginTop: 10
-    },
-    blueText: {
-        color: 'blue'
-    }
+	container: {
+		flex: 1,
+		marginTop: StatusBar.currentHeight
+	},
+	main: {
+		alignItems: 'center',
+		marginTop: 65, // (45 + 20)
+		padding: 13
+	},
+	image: {
+		width: 120,
+		height: 120
+	},
+	large: {
+		letterSpacing: 2,
+		fontSize: 20,
+		marginTop: 10
+	},
+	small: {
+		fontSize: 15,
+		padding: 13,
+		textAlign: 'center'
+	},
+	text: {
+		marginTop: 10
+	},
+	blueText: {
+		color: 'blue'
+	},
+	title: {
+		fontSize: 18,
+		margin: 8,
+		marginBottom: height / 38,
+		textAlign: 'center'
+	},
+	description: {
+		marginHorizontal: width / 16,
+		marginBottom: height / 38,
+		fontSize: 15,
+		textAlign: 'center'
+	},
+	divider: {
+		borderBottomWidth: 1,
+		borderBottomColor: 'green',
+		width: 350
+	},
+	socialItem: {
+		height: 80,
+		width: 80
+	},
+	social: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		padding: height / 40,
+		paddingTop: 0
+	}
 });
 
 //making About Container Available to other parts of App
