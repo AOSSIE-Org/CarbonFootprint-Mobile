b811b246ad7268e7b2410548453e1b0c585152ed
prudhvir3ddy <prudhvireddy.m01@gmail.com>
2020-01-03 21:55:53 +0530

    add sample keys

---
+++ app/config/keys.sample.js
@@ -0,0 +1,29 @@
+export const firebaseConfig = {
+    apiKey: null,
+    authDomain: null,
+    databaseURL: null,
+    projectId: null,
+    storageBucket: null,
+    messagingSenderId: null
+};
+
+export const firebaseConfigForTesting = {
+    apiKey: null,
+    authDomain: null,
+    databaseURL: null,
+    projectId: null,
+    storageBucket: null,
+    messagingSenderId: null
+};
+
+export const googleSignInConfig = {
+    webClientId: null
+};
+
+export const twitterSignInConfig = {
+    twitter_key: null,
+    twitter_secret: null
+};
+
+export const googleRoadsAPIKey = null;
+export const geocodingAPIKey = null;
