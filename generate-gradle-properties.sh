b9bb6aea6487aa5cc169a7799414965bf64b4e0e
Ritesh Agrawal <riteshagrawal1998@gmail.com>
2020-01-27 02:42:45 +0000

    [FIX] automatic closing of dialogbox
    Delete local.properties
    
    maintenance of project

---
+++ generate-gradle-properties.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cat  << EOF
+org.gradle.jvmargs=-Xmx4608M
+android.enableAapt2=true
+MYAPP_UPLOAD_STORE_FILE=keystore.jks
+MYAPP_UPLOAD_STORE_PASSWORD=$KEY_PASSWORD
+MYAPP_UPLOAD_KEY_ALIAS=$MY_ALIAS
+MYAPP_UPLOAD_KEY_PASSWORD=$KEY_PASSWORD
+EOF
